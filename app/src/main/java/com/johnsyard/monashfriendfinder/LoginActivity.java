f665123f32a375c2dff2447b23ff236ddd9d2e3f
XuanZhang <xuanzhang@XuanZhangdeMacBook-Pro.local>
2017-04-27 23:47:28 +1000

    finish login activity layout design

---
+++ app/src/main/java/com/johnsyard/monashfriendfinder/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.johnsyard.monashfriendfinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * This activity is used for login part
+ *
+ * Created by xuanzhang on 27/04/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button loginBtn = (Button) findViewById(R.id.bt_login);
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
+
